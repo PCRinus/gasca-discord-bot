ce158e18c10ffaf6748e2d295839050c851744f3
PCRinus <mircea.casapu@gmail.com>
2022-04-29 04:51:49 +0300

    feat: add dm sending feature

--- src/commands/gasca.ts
+++ src/commands/gasca.ts
@@ -1,17 +1,36 @@
 import { SlashCommandBuilder } from "@discordjs/builders";
+import { MessageEmbed, User } from "discord.js";
 import { ICommand } from "../interfaces/ICommand";
 
 export const gasca: ICommand = {
   data: new SlashCommandBuilder()
     .setName("gasca")
     .setDescription("The tagged user is a gâscă")
-    .addUserOption((option) =>
+    .addMentionableOption((option) =>
       option.setName("user").setDescription("The user which is a gâscă")
     ),
   run: async (interaction) => {
     await interaction.deferReply();
     const { user } = interaction;
-    console.log(interaction);
-    // const gascaUser = interaction.options.getMentionable()
+    //TODO: assign a User object to get schema validation and remove ts-ignore checks.
+    const gascaUser = interaction.options.getMentionable("user");
+
+    //@ts-ignore
+    gascaUser.send("Tata, ești o gâscă!");
+
+    console.log("Gasca", gascaUser);
+
+    const gascaEmbed = new MessageEmbed();
+    gascaEmbed
+      .setTitle("Tata, ești o gâscă!")
+      .setAuthor({
+        //@ts-ignore
+        name: gascaUser.user.tag,
+        //@ts-ignore
+        iconURL: gascaUser.user.displayAvatarURL(),
+      })
+      .addField("Cu drag: ", user.tag);
+
+    await interaction.editReply({ embeds: [gascaEmbed] });
   },
 };
